fc628f57285ffd327595cd9813af46fbb03d4d0b
Temujin <tjboii04@gmail.com>
2023-04-06 17:56:04 -0400

    Added helper functions

---
+++ noisefunctions.py
@@ -0,0 +1,114 @@
+import random
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from sklearn import preprocessing
+from tqdm import tqdm
+
+"""
+Functions for generating noise arrays (Fully Modular)
+------------------------------------------------------------
+Note:
+    When a new function is written, do not forget to add to noise_dict dictionary.
+"""
+
+def gauss_noise(df, key_col, std, seed):
+    """
+    Helper function that generates a gaussian curve of noises array
+    
+    Parameters:
+        df: pandas dataframe
+            Dataframe to read from
+        key_col: string
+            The key column of the property values to add noise to
+        std: float
+            Standard deviation for the gaussian curve to add noise from
+        seed: int
+            Seed for randomness
+        
+    Returns:
+        An array of gaussian noises
+    """
+    #Allows for reproducibility
+    np.random.seed(seed)
+
+    noise = np.random.normal(0, std, [len(df[key_col]),1], )
+    return np.concatenate(noise)    
+
+def const_bias(df, key_col, const, seed):
+    """
+    Helper function that generates constant noises (biases) array
+    
+    Parameters:
+        df: pandas dataframe
+            Dataframe to read from
+        key_col: string
+            The key column of the property values to add noise to
+        const: float
+            Constant value for the noise (bias)
+        seed: int
+            Not used
+        
+    Returns:
+        An array of constant noises
+    """
+    noise = [float(const) for index in range(len(df[key_col]))]
+    return noise
+
+def uniform_rand_noise(df, key_col, rand_range, seed):
+    """
+    Helper function that generates a purely random noise array
+    
+    Parameters:
+        df: pandas dataframe
+            Dataframe to read from
+        key_col: string
+            The key column of the property values to add noise to
+        rand_range: list
+            Range of allowed random numbers
+        seed: int
+            Seed for randomness   
+        
+    Returns:
+        An array of random noises within a range
+    """
+    #Allows for reproducibility
+    random.seed(seed)
+
+    noise = [random.uniform(rand_range[0],rand_range[1]) for index in range(len(df[key_col]))]
+    return noise
+
+def descriptor_bias(df, *descriptors):
+    """
+    Given a series of descriptors and corresponding coefficients,
+    calculates the weighted sum and returns it as a list of biases
+
+    Parameters:
+        df: pandas dataframe
+            Dataframe to read from
+        *descriptors: tuple
+            Variable amount of tuples of descriptor and weight in bias
+    
+    Returns:
+        Numpy array of biases calculated from the weighted sum of the descriptor
+        values
+    """
+    # Get mols objects from all the smiles indexes
+    mols = list(map(Chem.MolFromSmiles, df.index))
+    # Initialize the resulting array
+    result = np.zeros((1,len(df.index)))
+    # Iterate over the descriptor/coefficient pairs, normalize and add the 
+    # weighted result to the result array
+    for descriptor, coefficient in tqdm(descriptors):
+        pre_result = tuple(map(descriptor, mols.copy()))
+        result += np.array(preprocessing.normalize([pre_result]) * coefficient) 
+
+    return result
+    
+global noise_dict
+
+noise_dict = {
+        'constant':const_bias,
+        'gauss':gauss_noise,
+        'random':uniform_rand_noise
+    }
---
+++ splitfunctions.py
@@ -0,0 +1,171 @@
+import random
+from tqdm import tqdm
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from scaffold_splits import scaffold_split
+
+"""
+Functions for calculating the property value (Fully Modular)
+------------------------------------------------------------
+Note:
+    When a new function is written, do not forget to add to split_by_prop_dict dictionary.
+"""
+
+
+def molwt_split(df, cutoff_val: float = 150, seed: int = 0):
+    """
+     Function to split the dataframe into two sets where one is above the cutoff molecular weight and the other is below
+
+    Parameters:
+        df: pandas dataframe
+            Dataframe to fetch the smiles molecule
+        cutoff_val: float
+            Threshold value for splitting
+
+    Returns:
+        A tuple of lists containing the smiles representation and the corresponding molecular weight
+    """
+    # Allow for reproducibility
+    random.seed(seed)
+
+    # Initialize array
+    test, train_val = [], []
+
+    for entry in tqdm(df.index):
+
+        # Calculate molecular weight
+        mol = Chem.MolFromSmiles(entry)
+        entry_molwt = Descriptors.ExactMolWt(mol)
+
+        # If the property value of the current entry has a value over the cutoff then add it to the potential train_val set
+        if entry_molwt >= cutoff_val:
+            train_val.append(entry)
+        # Otherwise send it to the potential test set
+        else:
+            test.append(entry)
+
+    random.shuffle(train_val)
+    random.shuffle(test)
+
+    return (train_val, test)
+
+
+def h298_split(df, cutoff_val: float = 0, seed: int = 0):
+    """
+    Function to split the dataframe into two sets where one is above the cutoff h298 value and the other is below
+
+    Parameters:
+        df: pandas dataframe
+            Dataframe to find the h298 value of the given smiles molecule
+        cutoff_val: float
+            Threshold value for splitting
+
+    Returns:
+        A tuple of lists containing the smiles representation and the corresponding h298 value
+    """
+    # Allow for reproducibility
+    random.seed(seed)
+
+    # Initialize array
+    test, train_val = [], []
+
+    for index in tqdm(df.index):
+
+        # If the enthalpy of the current entry has a value over the cutoff then add it to the potential train_val set
+        if df["h298"][index] >= cutoff_val:
+            train_val.append(index)
+        # Otherwise send it to the potential test set
+        else:
+            test.append(index)
+
+    random.shuffle(train_val)
+    random.shuffle(test)
+
+    return (train_val, test)
+
+
+def atoms_to_exclude(df, restricted_list: list = ["F"], seed: int = 0):
+    """
+    Function to split the dataframe into two sets where one is above the cutoff h298 value and the other is below
+
+    Parameters:
+        df: pandas dataframe
+            Dataframe to find the h298 value of the given smiles molecule
+        cutoff_val: list
+            List containing disallowed atoms
+
+    Returns:
+        A tuple of lists containing smiles representations
+    """
+    # Allow for reproducibility
+    random.seed(seed)
+
+    # Initialize array
+    test, train_val = [], []
+
+    # Initialize symbol to atomic number dictionary (Expandable)
+    atom_dict = {"B": 5, "N": 7, "O": 8, "F": 9}
+
+    restricted = restricted_list[:]
+    # Translate list of disallowed atom strings into a list of atomic numbers
+    for index in range(len(restricted)):
+        restricted[index] = atom_dict[restricted[index]]
+
+    # Flag used for when atomic_num was in the list and the current entry need not be appended
+    # to the train_val as it is already in test
+    flag = False
+
+    for entry in tqdm(df.index):
+        flag = False
+        mol = Chem.MolFromSmiles(entry)
+        # Construct the molecule and iterate atom after atom
+        for atom in mol.GetAtoms():
+            atomic_num = atom.GetAtomicNum()
+
+            # If it matches any disallowed atomic numbers then append to test and move on to next iteration
+            if atomic_num in restricted:
+                test.append(entry)
+                flag = True
+                break
+        # Otherwise flag will be false and it can appended to train_val
+        if not flag:
+            train_val.append(entry)
+
+    random.shuffle(train_val)
+    random.shuffle(test)
+
+    return (train_val, test)
+
+
+def random_split(df, sizes: tuple = (0.9, 0.1), seed: int = 0):
+    """
+    Function to split the dataframe into two sets where one is above the cutoff h298 value and the other is below
+
+    Parameters:
+        df: pandas dataframe
+            Dataframe to split randomly
+        sizes: tuple
+            Proportions to split into
+
+    Returns:
+        A tuple of lists containing smiles representations
+    """
+    assert sum(sizes) == 1
+
+    train_val = list(df.sample(frac=sizes[0], random_state=seed).index)
+    test = list(df.drop(train_val).index)
+
+    random.shuffle(train_val)
+    random.shuffle(test)
+
+    return (train_val, test)
+
+global split_by_prop_dict
+
+split_by_prop_dict = {
+    "h298": h298_split,
+    "molwt": molwt_split,
+    "atom": atoms_to_exclude,
+    "random": random_split,
+    "scaffold": scaffold_split,
+}
