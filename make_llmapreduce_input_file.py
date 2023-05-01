7e19a329f6691db58420d9c673017731040c7066
Kevin Greenman <kpg@mit.edu>
2023-05-01 10:17:51 -0400

    more inputs

--- make_llmapreduce_input_file.py
+++ make_llmapreduce_input_file.py
@@ -10,7 +10,8 @@ for model_type in [
     for descriptor_bias in [0, 10, 50]:
         # for polynomial_bias in [0, 2, 3]:
         # for polynomial_bias in [1]:
-        for polynomial_bias in [0, 1, 2, 3]:
+        # for polynomial_bias in [0, 1, 2, 3]:
+        for polynomial_bias in [0]:
             for constant_bias in [0, 10, 50]:
                 for gauss_noise in [0, 10, 50]:
                     for split_type in [
@@ -21,11 +22,11 @@ for model_type in [
                         # "atom",
                     ]:
                         # for lf_hf_size_ratio in [1, 10]:
-                        # for lf_hf_size_ratio in [100]:
-                        for lf_hf_size_ratio in [1, 10, 100]:
+                        # for lf_hf_size_ratio in [1, 10, 100]:
+                        for lf_hf_size_ratio in [100]:
                             for lf_superset_of_hf in [
-                                # True,
-                                False,
+                                True,
+                                # False,
                             ]:
                                 command_str = f"{i:03} {model_type} {descriptor_bias} {polynomial_bias} {constant_bias} {gauss_noise} {split_type} {lf_hf_size_ratio} {lf_superset_of_hf}"
                                 command_str_list.append(command_str)
@@ -33,7 +34,8 @@ for model_type in [
 
 # with open("inputs.txt", "w") as f:
 # with open("inputs2.txt", "w") as f:
-with open("inputs3.txt", "w") as f:
+# with open("inputs3.txt", "w") as f:
+with open("inputs4.txt", "w") as f:
     for i, command_str in enumerate(command_str_list):
         if i == 0:
             f.write(command_str)
