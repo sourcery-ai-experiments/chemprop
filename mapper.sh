924ed995550be9a80aac7d62f2e381bc62a2d836
Temujin Orkhon <125429179+iceteaj@users.noreply.github.com>
2023-10-10 00:10:50 -0400

    Update mapper.sh

--- mapper.sh
+++ mapper.sh
@@ -2,7 +2,7 @@
 
 source /etc/profile
 module load anaconda/2023a
-source activate chempropmf
+source activate chemprop
 
 echo "Model: " $2
 echo "Descriptor Bias: " $3
@@ -17,7 +17,7 @@ echo "Seed: " ${10}
 
 python multifidelity_end2end.py \
 --model_type $2 \
---data_file /home/gridsan/torkhon/chemprop-mf/tests/data/gdb11_0.001.csv \
+--data_file /home/gridsan/kgreenman/mf_benchmark/chemprop/tests/data/gdb11_0.001.csv \
 --hf_col_name h298 \
 --lf_col_name h298_lf \
 --scale_data True \
