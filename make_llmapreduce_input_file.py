dd8f573d544e07cfd4c49b84f67be3dff9ce487e
Temujin <temujin@mit.edu>
2023-11-06 20:26:12 -0500

    sorted inputs

--- make_llmapreduce_input_file.py
+++ make_llmapreduce_input_file.py
@@ -7,20 +7,17 @@ i = 3
 
 for seed in [0, 1, 2]:
     for model_type in [
-        "multi_target",
-        "multi_fidelity",
-        "multi_fidelity_weight_sharing",
+        "delta_ml",
+        "trad_delta_ml"
     ]:
         for split_type in [
-            "random",
-            # "scaffold",
-            # "h298",
-            # "molwt",
-            # "atom",
+            "scaffold",
+            "h298",
+            "molwt",
+            "atom",
         ]:
-            for lf_hf_size_ratio in [1, 10, 100]:
+            for lf_hf_size_ratio in [1]:
                 for lf_superset_of_hf in [
-                    True,
                     False,
                 ]:
                     for descriptor_bias in [0, 10, 50]:
@@ -40,9 +37,9 @@ for seed in [0, 1, 2]:
                         command_str_list.append(command_str)
                         i += 1
 
-command_str_list = list(set(command_str_list))
+command_str_list = sorted(list(set(command_str_list)))
 
-with open("inputs_orthogonal.txt", "w") as f:
+with open("inputs_delta2.txt", "w") as f:
     for i, command_str in enumerate(command_str_list):
         if i == 0:
             f.write(command_str)
