42e146a6e815b655185fd78eb17d8eec5fa03a8c
Kevin Greenman <kpg@mit.edu>
2024-02-14 01:36:18 -0500

    new inputs for real datasets after fixes

--- make_llmapreduce_input_file_datasets_pc.py
+++ make_llmapreduce_input_file_datasets_pc.py
@@ -5,15 +5,15 @@ command_str_list = [
     "003 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 scaffold 1 True 0",
     "004 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 scaffold 1 True 1",
     "005 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 scaffold 1 True 2",
-    "006 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 h298 1 True 0",
-    "007 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 h298 1 True 1",
-    "008 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 h298 1 True 2",
-    "009 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 molwt 1 True 0",
-    "010 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 molwt 1 True 1",
-    "011 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 molwt 1 True 2",
-    "012 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 atom 1 True 0",
-    "013 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 atom 1 True 1",
-    "014 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 atom 1 True 2",
+    # "006 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 h298 1 True 0",
+    # "007 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 h298 1 True 1",
+    # "008 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 h298 1 True 2",
+    # "009 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 molwt 1 True 0",
+    # "010 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 molwt 1 True 1",
+    # "011 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 molwt 1 True 2",
+    # "012 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 atom 1 True 0",
+    # "013 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 atom 1 True 1",
+    # "014 single_fidelity pubchem_AID1445_sd_dr_0.1 0 0 0 0 atom 1 True 2",
 ]
 i = 15
 
@@ -22,12 +22,12 @@ for seed in [0, 1, 2]:
         # "single_fidelity",
         "multi_target",
         "multi_fidelity",
-        "delta_ml",
+        # "delta_ml",
         # "trad_delta_ml",
         "transfer",
         # "multi_fidelity_weight_sharing",
     ]:
-        for split_type in ["random", "scaffold", "h298", "molwt", "atom"]:
+        for split_type in ["random", "scaffold"]:  # , "h298", "molwt", "atom"]:
             for lf_hf_size_ratio in [1]:  # , 10, 100]:
                 for lf_superset_of_hf in [
                     True,
