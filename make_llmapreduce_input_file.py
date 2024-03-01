4670744421af41bb97f9a76bfe0e7ed3903e4299
Temujin <temujin@mit.edu>
2024-03-01 09:12:59 -0500

    Bug fixes

--- make_llmapreduce_input_file.py
+++ make_llmapreduce_input_file.py
@@ -1,11 +1,12 @@
 command_str_list = []
-
+i=0
 for seed in [0, 1, 2]:
     for model_type in ["multi_fidelity", "evidentialmf", "mvemultifidelity"]:
         for loss_mod in [2, 5, 10, 30, 50]:
             for gauss_noise in [5]:
-                command_str = f"{model_type} {loss_mod} {seed} {gauss_noise}"
+                command_str = f"{i} {model_type} {loss_mod} {seed} {gauss_noise}"
                 command_str_list.append(command_str)
+                i += 1
 
 
 with open("inputs_unc.txt", "w") as f:
